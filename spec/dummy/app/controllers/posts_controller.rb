1f0b4e2b9cbe9d5759e2ff29bbaad2f1f4d1b7c2
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2017-06-04 18:23:29 +0200

    DRY controllers

--- spec/dummy/app/controllers/posts_controller.rb
+++ spec/dummy/app/controllers/posts_controller.rb
@@ -1,25 +1,20 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
   def create
     @post = Post.new(post_params)
 
@@ -30,7 +25,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
       redirect_to @post, notice: 'Post was successfully updated.'
@@ -39,19 +33,17 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
   def destroy
     @post.destroy
     redirect_to posts_url, notice: 'Post was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def post_params
       params.require(:post).permit(:title, :content, :user_id)
     end
